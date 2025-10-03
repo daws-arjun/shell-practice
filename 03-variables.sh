3c997f24e1ff0f22545e1c09c9759a710ba428be
daws-arjun <a.nagarjuna97@gmail.com>
2025-10-03 13:22:10 +0530

    latest changes

--- 03-variables.sh
+++ 03-variables.sh
@@ -1,19 +1,51 @@
-#!/bin/bash/
+#!/bin/bash
 
 # Variable assignment (no spaces around '=')
 PERSON1=Nani
 PERSON2=Mahesh
+PERSON3=Ramesh
+PERSON4=Suresh
 
 CITY1=Bangalore
 CITY2=Hyderabad
 
-# echo "$PERSON1 :: Hi $PERSON2, how are you?"
-# echo "$PERSON2 :: Hello $PERSON1, I am fine, how are you?"
-# echo "$PERSON1 :: I am fine, What are you learning recently"
-# echo "$PERSON2 :: I am learning shell scripting $PERSON1"
-# echo "$PERSON1 :: Oh! nice"
+PLACE1=MG-Road
+PLACE2=Charminar
 
-echo "$PERSON1 :: Hello $PERSON2 where are you going today"
-echo "$PERSON2 :: Hi $PERSON1, today I am going to $CITY1. When will you come to $CITY1?"
-echo "$PERSON1 :: Currently, I am in $CITY2. I will come to $CITY1 tomorrow."
-echo "$PERSON2 :: That’s good! We can meet in $CITY1 and spend some time together"
+COMPANY1=Infosys
+COMPANY2=TCS
+
+# Conversation between person1 and person2
+
+echo "$PERSON1 :: Hi $PERSON2, how are you?"
+echo "$PERSON2 :: Hello $PERSON1, I am fine. How are you?"
+echo "$PERSON1 :: I am good. What are you learning recently?"
+echo "$PERSON2 :: I am learning shell scripting, $PERSON1."
+echo "$PERSON1 :: Oh, that’s great! Keep it up."
+echo "$PERSON2 :: Thank you $PERSON1. By the way, where are you today?"
+echo "$PERSON1 :: Right now, I am in $CITY2 near $PLACE2."
+echo "$PERSON2 :: Nice! I am in $CITY1, working at $COMPANY1."
+echo "$PERSON1 :: Oh really? $PERSON3 also works at $COMPANY1 in $CITY1."
+echo "$PERSON2 :: Yes, I met $PERSON3 last week. He is doing well."
+echo "$PERSON1 :: That’s good to know. When are you planning to visit $CITY2?"
+echo "$PERSON2 :: Maybe next month. I will also visit $PLACE2 in $CITY2."
+echo "$PERSON1 :: Perfect! We can go together to $PLACE2 and try Hyderabadi biryani."
+echo "$PERSON2 :: Haha, sure! I love biryani from Hyderabad."
+echo "$PERSON1 :: By the way, do you know $PERSON4? He recently joined $COMPANY2 in $CITY2."
+echo "$PERSON2 :: Oh yes, I heard about him. He shifted from $COMPANY1 to $COMPANY2, right?"
+echo "$PERSON1 :: Exactly. He said $COMPANY2 has better projects in cloud and DevOps."
+echo "$PERSON2 :: That’s interesting. Are you also planning to switch companies?"
+echo "$PERSON1 :: Not now. But maybe in future, if I get a good opportunity."
+echo "$PERSON2 :: Makes sense. Anyway, when are you coming to $CITY1?"
+echo "$PERSON1 :: Tomorrow, I will reach $CITY1 by evening."
+echo "$PERSON2 :: That’s great! Let’s meet at $PLACE1 in $CITY1."
+echo "$PERSON1 :: Sure! We can also invite $PERSON3 and $PERSON4 for dinner."
+echo "$PERSON2 :: Good idea. We can book a table at a nice restaurant near $PLACE1."
+echo "$PERSON1 :: Sounds perfect. After dinner, maybe we can visit some spots in $CITY1."
+echo "$PERSON2 :: Yes, we can. $CITY1 has a lot of cool places to hang out."
+echo "$PERSON1 :: True. But don’t forget, next month you are coming to $CITY2."
+echo "$PERSON2 :: Haha, of course! I will definitely come and explore $PLACE2 again."
+echo "$PERSON1 :: Okay then, see you tomorrow in $CITY1."
+echo "$PERSON2 :: Sure $PERSON1, safe travels from $CITY2 to $CITY1."
+echo "$PERSON1 :: Thanks $PERSON2. Bye for now!"
+echo "$PERSON2 :: Bye $PERSON1, see you soon!"
