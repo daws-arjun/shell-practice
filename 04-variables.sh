e763698dea02353867edb181da2732e1b6ff309f
daws-arjun <a.nagarjuna97@gmail.com>
2025-10-03 15:07:45 +0530

    latest changes

--- 04-variables.sh
+++ 04-variables.sh
@@ -3,7 +3,7 @@
 PERSON1=$1
 PERSON2=$2
 
-echo "$PERSON1: Welcome to Alaska, $PERSON2."
-echo "$PERSON2: Thank you, $PERSON1. Glad we're not on a Zoom call this time."
-echo "$PERSON1: So, do we have a deal?"
-echo "$PERSON2: Yes — but only if Ukraine becomes part of Russia."
+echo "$PERSON1:: Welcome to Alaska, $PERSON2."
+echo "$PERSON2:: Thank you, $PERSON1. Glad we're not on a Zoom call this time."
+echo "$PERSON1:: So, do we have a deal?"
+echo "$PERSON2:: Yes — but only if Ukraine becomes part of Russia."
