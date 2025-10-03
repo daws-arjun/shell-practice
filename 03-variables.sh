6a826ad9fb9bd8ab404eb90a109d4c932822b0f1
daws-arjun <a.nagarjuna97@gmail.com>
2025-10-03 13:15:04 +0530

    latest changes

--- 03-variables.sh
+++ 03-variables.sh
@@ -1,11 +1,19 @@
 #!/bin/bash/
 
-# No space before or after equal (=)
-PERSON1=Kohli
-PERSON2=Dhoni
+# Variable assignment (no spaces around '=')
+PERSON1=Nani
+PERSON2=Mahesh
+
+CITY1=Bangalore
+CITY2=Hyderabad
 
 echo "$PERSON1 :: Hi $PERSON2, how are you?"
 echo "$PERSON2 :: Hello $PERSON1, I am fine, how are you?"
 echo "$PERSON1 :: I am fine, What are you learning recently"
 echo "$PERSON2 :: I am learning shell scripting $PERSON1"
 echo "$PERSON1 :: Oh! nice"
+
+echo "$PERSON1 :: Hello $PERSON2 where are you going today"
+echo "$PERSON2 :: Hi $PERSON1, today I am going to $CITY1. When will you come to $CITY1?"
+echo "$PERSON1 :: Currently, I am in $CITY2. I will come to $CITY1 tomorrow."
+echo "$PERSON2 :: That’s good! We can meet in $CITY1 and spend some time together"
