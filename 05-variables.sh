2dcadd24f2fae10eb815b179226f8c155e051a62
daws-arjun <a.nagarjuna97@gmail.com>
2025-10-03 15:36:35 +0530

    latest changes

--- 05-variables.sh
+++ 05-variables.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 echo "Please enter your PIN NUMBER"
-read -s PIN_NUMBER # here PIN_NUMBER is variable
+read -p PIN_NUMBER # here PIN_NUMBER is variable
 
 echo "PIN entered is: $PIN_NUMBER"  #Testing purpose we are printing, We are not printing password/PINnumbers 
