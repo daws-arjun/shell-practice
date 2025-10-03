cd93dc145713246f857d59f2bae3e7bbcb47b89d
daws-arjun <a.nagarjuna97@gmail.com>
2025-10-03 12:42:04 +0530

    latest changes

--- 03-variables.sh
+++ 03-variables.sh
@@ -4,8 +4,8 @@
 PERSON1=John
 PERSON2=Raheem
 
-echo "$PERSON1 :: Hi $PERSON2, How are you?"
-echo "$PERSON2 :: Hello $PERSON1, I am fine, How are you?"
+echo "$PERSON1 :: Hi $PERSON2, how are you?"
+echo "$PERSON2 :: Hello $PERSON1, I am fine, how are you?"
 echo "$PERSON1 :: I am fine, What are you learning recently"
-echo "$PERSON2 :: I am learing shell scripting $PERSON1"
+echo "$PERSON2 :: I am learning shell scripting $PERSON1"
 echo "$PERSON1 :: Oh! nice"
